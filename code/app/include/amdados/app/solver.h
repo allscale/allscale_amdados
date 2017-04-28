4f5251c1170f41232ca15e7f8f1ebdce697326e9
Fearghal O'Donncha <feardonn@ie.ibm.com>
2017-04-28 12:14:20 +0100

    Updated description of solver outline in Solver.h

--- code/app/include/amdados/app/solver.h
+++ code/app/include/amdados/app/solver.h
@@ -9,6 +9,17 @@
 namespace amdados {
 namespace app {
 
+// steps for the solver;
+// 1) Need to initialize all domains to zero (& all layers I would think L100, L20 and L4)
+// 2) Need to distribute work across processors for solution of advection diffusion
+//      In this manner it would be relatively straightforward task to implement the solver within each subdomain
+// 3) Need to integrate data assimilation schemes from kalman_filter.h and filter.h
+//      Data structures are in place and simply needs to be integrated with appropriate data from advection-diffusion process
+// 4) Need to integrate adaptive meshing capabilities
+//      Right now this links with (3): If data is available then resolve solution to higher fidelity grid (down to 20m) and
+//      assimilate at this scale. Then solution returns to 100m grid and solution continues. For time being; advection-diffusion
+//      always happens on the 100m grid and we resolve at higher resolution only for data assimilation
+
 // --- Initialize ---
   // pfor(zero, size, [&](const  allscale::api::user::data::GridPoint<2>& pos) {
 
